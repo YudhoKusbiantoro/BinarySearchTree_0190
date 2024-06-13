d4687bb9192720576127e26f29af5ef553f580c8
YudhoKusbiantoro <masakan.padang1118@gmail.com>
2024-06-13 13:50:01 +0700

    Membuat prosedur postorder

--- Binary Searh Tree/Binary Searh Tree.cpp
+++ Binary Searh Tree/Binary Searh Tree.cpp
@@ -101,3 +101,20 @@ public:
 			preorder(ptr->rightchild);
 		}
 	}
+	void postorder(Node* ptr)
+	{
+		//perfom the postorder tranversal of the tree
+		if (ROOT == nullptr)
+		{
+			cout << "Tree is empty" << endl;
+			return;
+		}
+		if (ptr != nullptr)
+		{
+			postorder(ptr->leftchild);
+			postorder(ptr->rightchild);
+			cout << ptr->info << " ";
+		}
+	}
+};
+
