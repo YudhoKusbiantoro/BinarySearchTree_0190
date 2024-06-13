8bbaa931d9abb6a14b26aaaa547d11314b070be0
YudhoKusbiantoro <masakan.padang1118@gmail.com>
2024-06-13 12:55:30 +0700

    Membuat kelas Node

--- Binary Searh Tree/Binary Searh Tree.cpp
+++ Binary Searh Tree/Binary Searh Tree.cpp
@@ -8,27 +8,3 @@ using namespace std;
 
 class Node
 {
-public:
-	string info;
-	Node* leftchild;
-	Node* rightchild;
-
-	//Constructor for the node class
-	Node(string i, Node* l, Node* r)
-	{
-		info = i;
-		leftchild = l;
-		rightchild = r;
-	}
-};
-
-class BinaryTree
-{
-public:
-	Node* ROOT;
-
-	BinaryTree()
-	{
-		ROOT = 
-	}
-};
