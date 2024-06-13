7c6192ad7a1f2f2fbec0fee717aad3d41e8db38b
YudhoKusbiantoro <masakan.padang1118@gmail.com>
2024-06-13 13:10:14 +0700

    Mencari node lokasi node baru untuk menjadi node pertama

--- Binary Searh Tree/Binary Searh Tree.cpp
+++ Binary Searh Tree/Binary Searh Tree.cpp
@@ -39,3 +39,7 @@ public:
 		newNode->leftchild = nullptr; // Make the left child of the new node point to NULL
 		newNode->rightchild = nullptr; //Make the right child of the new node point to NULL
 
+		Node* parent = nullptr;
+		Node* currentNode = nullptr;
+		search(element, parent, currentNode); // Locate the node which will be the parent of the node to be insert
+
